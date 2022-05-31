d996bf6f7a6851d82771597d5ccf98d91a4d0243
Sharlatan Hellseher <sharlatanus@gmail.com>
2022-06-01 00:21:50 +0100

    Sync maths packages

--- ffab/packages/maths.scm
+++ ffab/packages/maths.scm
@@ -1,3 +1,21 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2021-2022 Sharlatan Hellseher <sharlatanus@gmail.com>
+;;;
+;;; This file is NOT part of GNU Guix.
+;;;
+;;; This program is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 (define-module (ffab packages maths)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (gnu packages compression)
@@ -31,119 +49,76 @@
   #:use-module (guix build-system gnu)
   #:use-module (guix build-system python)
   #:use-module (guix download)
+  #:use-module (guix gexp)
   #:use-module (guix packages))
 
 ;(define-public plplot-base)
 
 (define-public plplot
   (package
-   (name "plplot")
-   (version "5.15.0")
-   (source (origin
-            (method git-fetch)
-            (uri (git-reference
-                  ;; Mirror: https://github.com/PLplot/PLplot
-                  (url "https://git.code.sf.net/p/plplot/plplot")
-                  (commit (string-append name "-" version))))
-            (sha256
-             (base32
-              "0fn9j251zv9pwlqy30yv4flwcd0cbyxd1nn388n1k158jycw2i91"))
-            (file-name (git-file-name name version))))
-   (build-system cmake-build-system)
-   (arguments
-    ;; NOTE: (Sharlatan-20210204T120351+0000): Tests failing on examples_cxx
-    `(#:configure-flags
-      (let ((out (assoc-ref %outputs "out")))
-        (list "-DBUILD_TEST=ON" "-DCMAKE_BUILD_TYPE=Release"
-              "-DCMAKE_SKIP_BUILD_RPATH=OFF"
-              (string-append "-DCMAKE_INSTALL_LIBDIR=" out "/lib")
-              (string-append "-DCMAKE_INSTALL_PREFIX=" out)))))
-   (native-inputs
-    (list ocaml
-          perl
-          python-wrapper
-          pkg-config
-          swig))
-   (inputs
-    (list freetype
-          gfortran
-          libltdl
-          lua
-          octave
-          pango
-          perl-xml-dom
-          perl-xml-parser
-          python-numpy
-          qhull
-          shapelib
-          tcl
-          tk
-          wxwidgets))
-   (home-page "http://plplot.org/")
-   (synopsis "Scientific graphics plotting library")
-   (description
-    "PLplot is a cross-platform software package for creating scientific plots
-whose (UTF-8) plot symbols and text.")
-   (license license:lgpl2.0))) ; Other terms are in Copyright file
-
-(define-public veusz
-  (package
-    (name "veusz")
-    (version "3.4")
+    (name "plplot")
+    (version "5.15.0")
     (source
      (origin
-       (method url-fetch)
-       (uri (pypi-uri "veusz" version))
+       (method git-fetch)
+       (uri (git-reference
+             ;; Mirror: https://github.com/PLplot/PLplot
+             (url "https://git.code.sf.net/p/plplot/plplot")
+             (commit (string-append name "-" version))))
        (sha256
-        (base32 "0p6cv7ppk2cs8m13wqa527hzybdmd3lmshw331kxpz0wvykv17yd"))))
-    (build-system python-build-system)
+        (base32
+         "0fn9j251zv9pwlqy30yv4flwcd0cbyxd1nn388n1k158jycw2i91"))
+       (file-name (git-file-name name version))))
+    (build-system cmake-build-system)
     (arguments
-     `(#:phases
-       (modify-phases %standard-phases
-         ;; Veusz will append 'PyQt5' to sip_dir by default. That is not how
-         ;; the path is defined in Guix, therefore we have to change it.
-         (add-after 'unpack 'fix-sip-dir
-           (lambda _
-             (substitute* "pyqtdistutils.py"
-               (("os.path.join\\(sip_dir, 'PyQt5'\\)") "sip_dir"))
-             #t))
-         ;; Now we have to pass the correct sip_dir to setup.py.
-         (replace 'build
-           (lambda* (#:key inputs #:allow-other-keys)
-             ;; We need to tell setup.py where to locate QtCoremod.sip
-             ((@@ (guix build python-build-system) call-setuppy)
-              "build_ext"
-              (list (string-append "--sip-dir="
-                                   (assoc-ref inputs "python-pyqt")
-                                   "/share/sip"))
-              #t)))
-         ;; Ensure that icons are found at runtime.
-         (add-after 'install 'wrap-executable
-           (lambda* (#:key inputs outputs #:allow-other-keys)
-             (let ((out (assoc-ref outputs "out")))
-               (wrap-program (string-append out "/bin/veusz")
-                 `("QT_PLUGIN_PATH" prefix
-                   ,(list (string-append (assoc-ref inputs "qtsvg")
-                                         "/lib/qt5/plugins/"))))))))))
+     (list
+      #:configure-flags
+      #~(list
+         "-DBUILD_TEST=ON"
+         (string-append "-DCMAKE_INSTALL_LIBDIR=" #$output "/lib")
+         (string-append "-DCMAKE_INSTALL_PREFIX=" #$output))))
     (native-inputs
-     `(("pkg-config" ,pkg-config)
-       ("qttools" ,qttools)
-       ("python-sip" ,python-sip-4)))
+     (list perl
+           perl-xml-dom
+           perl-xml-parser
+           pkg-config
+           ;python-wrapper
+           swig))
     (inputs
-     `(("ghostscript" ,ghostscript) ;optional, for EPS/PS output
-       ("python-astropy" ,python-astropy)
-       ("python-dbus" ,python-dbus)
-       ("python-h5py" ,python-h5py) ;optional, for HDF5 data
-       ("python-pyqt" ,python-pyqt)
-       ("qtbase" ,qtbase-5)
-       ("qtsvg" ,qtsvg)))
-    (propagated-inputs
-     `(("python-numpy" ,python-numpy)))
-    (home-page "https://veusz.github.io/")
-    (synopsis "Scientific plotting package")
+     (list freetype
+           camlidl
+           gfortran
+           libltdl
+           lua
+           ocaml
+           octave
+           pango
+           python-numpy
+           python-wrapper
+           qhull
+           shapelib
+           tcl
+           tk
+           wxwidgets))
+    (home-page "http://plplot.org/")
+    (synopsis "Scientific graphics plotting library")
     (description
-     "Veusz is a scientific plotting and graphing program with a graphical
-user interface, designed to produce publication-ready 2D and 3D plots.  In
-addition it can be used as a module in Python for plotting.  It supports
-vector and bitmap output, including PDF, Postscript, SVG and EMF.")
-    (license license:gpl2+)))
+     "PLplot is a cross-platform software package for creating scientific plots
+whose (UTF-8) plot symbols and text.
+
+ The PLplot core library can be used to create standard x-y plots, semi-log
+plots, log-log plots, contour plots, 3D surface plots, mesh plots, bar charts
+and pie charts.  Multiple graphs (of the same or different sizes) may be placed
+on a single page, and multiple pages are allowed for those device formats that
+support them.
+
+PLplot has core library support for plot symbols and text specified by the user
+in the UTF-8 encoding of Unicode.  This means for our many Unicode-aware devices
+that plot symbols and text are only limited by the collection of glyphs normally
+available via installed system fonts.  Furthermore, a large subset of our
+Unicode-aware devices also support complex text layout (CTL) languages such as
+Arabic, Hebrew, and Indic and Indic-derived CTL scripts such as Devanagari,
+Thai, Lao, and Tibetan.  Thus, for these PLplot devices essentially any language
+that is supported by Unicode and installed system fonts can be used to label
+plots.")
+    (license license:lgpl2.0))) ; Other terms are in Copyright file
