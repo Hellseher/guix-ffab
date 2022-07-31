be25de071f56d5af24eb2f9b4dfcc1c134c2c0bd
Sharlatan Hellseher <sharlatanus@gmail.com>
2022-07-31 22:21:42 +0100

    ffab: Clean up accepted: hashicorp/hcl-v1

--- ffab/packages/golang.scm
+++ ffab/packages/golang.scm
@@ -130,32 +130,9 @@ replacement.  Running @code{gofmt} after @code{gofumpt} should be a no-op.")
     (license license:bsd-3)))
 
 ;; 20220731T214024+0100
-(define-public go-github-com-hashicorp-hcl
-  (package
-    (name "go-github-com-hashicorp-hcl")
-    (version "1.0.0")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://github.com/hashicorp/hcl")
-             (commit (string-append "v" version))))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32
-         "0q6ml0qqs0yil76mpn4mdx4lp94id8vbv575qm60jzl1ijcl5i66"))))
-    (build-system go-build-system)
-    (arguments
-     '(#:import-path "github.com/hashicorp/hcl"))
-    (native-inputs
-     `(("go-github-com-davecgh-go-spew" ,go-github-com-davecgh-go-spew)))
-    (synopsis "Go implementation of HashiCorp Configuration Language V1")
-    (description
-     "This package contains the main implementation of the @acronym{HCL,
-HashiCorp Configuration Language}.  HCL is designed to be a language for
-expressing configuration which is easy for both humans and machines to read.")
-    (home-page "https://github.com/hashicorp/hcl")
-    (license license:mpl2.0)))
+;; (define-public go-github-com-hashicorp-hcl
+;; added-to-upstream: 97657689507a0681aa9699407d292383ff22c330
+;; CommitDate: Wed Jan 5 23:44:20 2022 +0100
 
 ;; <2021-12-12 Sun>
 ;; (define-public go-github-com-hashicorp-hcl-v2
