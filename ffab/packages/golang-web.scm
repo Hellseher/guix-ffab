efbcb1d16d58642a07f923d86c99062f639e1c3f
Sharlatan Hellseher <sharlatanus@gmail.com>
2022-08-02 12:49:48 +0100

    ffab: Move git related packages to it's module
    * ffab/packages/golang.scm: (lazygit) Move from
      ffab/packages/version-control.scm: (lazygit) Move to
    
    * ffab/packages/golang.scm: (github-cli) Move from
      ffab/packages/version-control.scm: (github-cli) Move to
    
    * ffab/packages/golang-web.scm: Fix module name.

--- ffab/packages/golang-web.scm
+++ ffab/packages/golang-web.scm
@@ -16,7 +16,7 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-(define-module (ffab packages golang)
+(define-module (ffab packages golang-web)
   #:use-module (ffab packages protobuf)
   #:use-module (ffab packages golang)
   #:use-module ((guix licenses) #:prefix license:)
---
+++ ffab/packages/version-control.scm
@@ -0,0 +1,164 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2022 Sharlatan Hellseher <sharlatanus@gmail.com>
+;;;
+;;; This file is NOT part of GNU Guix.
+;;;
+;;; This program is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (ffab packages version-control)
+  #:use-module (ffab packages golang)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages golang)
+  #:use-module (gnu packages check)
+  #:use-module (gnu packages vim)
+  #:use-module (guix build-system go)
+  #:use-module (guix download)
+  #:use-module (guix gexp)
+  #:use-module (guix git-download)
+  #:use-module (guix packages))
+
+
+;;+begin_github.com/cli
+
+;; 20220730T151450+0100
+(define-public github-cli
+  (package
+    (name "github-cli")
+    (version "2.3.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/cli/cli")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0qf19rkckbfwcsk9rkfnbrzrksb6r50p7gda25lbw86n2c3k18wp"))))
+    (build-system go-build-system)
+    ;; (arguments
+    ;;  '(#:unpack-path "github.com/cli/cli/v2"
+    ;;    #:import-path "github.com/cli/cli/v2"))
+    (native-inputs
+     (list go-github-com-alecaivazis-survey-v2
+           go-github-com-briandowns-spinner
+           go-github-com-charmbracelet-glamour
+           go-github-com-cli-browser
+           go-github-com-cli-oauth
+           go-github-com-cli-safeexec
+           go-github-com-cli-shurcool-graphql
+           go-github-com-cpuguy83-go-md2man-v2
+           go-github-com-creack-pty
+           go-github-com-gabriel-vasile-mimetype
+           go-github-com-google-go-cmp
+           go-github-com-google-shlex
+           go-github-com-hashicorp-go-version
+           go-github-com-henvic-httpretty
+           go-github-com-itchyny-gojq
+           go-github-com-kballard-go-shellquote
+           go-github-com-makenowjust-heredoc
+           go-github-com-mattn-go-colorable
+           go-github-com-mattn-go-isatty
+           go-github-com-mattn-go-runewidth
+           go-github-com-mgutz-ansi
+           go-github-com-mitchellh-go-homedir
+           go-github-com-muesli-termenv
+           go-github-com-rivo-uniseg
+           go-github-com-shurcool-githubv4
+           go-github-com-spf13-cobra
+           go-github-com-spf13-pflag
+           go-github-com-stretchr-objx
+           go-github-com-stretchr-testify
+           go-golang-org-x-crypto
+           go-golang-org-x-sync
+           go-golang-org-x-sys
+           go-golang-org-x-term
+           go-gopkg-in-yaml-v3))
+    (home-page "https://github.com/cli/cli")
+    (synopsis "GitHub CLI")
+    (description
+     "@code{gh} is GitHub on the command line. It brings pull requests, issues,
+and other GitHub concepts to the terminal next to where you are already working
+with @code{git} and your code.")
+    (license license:expat)))
+
+;;+begin_github.com/jesseduffield
+;;
+;; Some packages are introduced in:
+;; - github.com/cli/cli: https://issues.guix.gnu.org/47539
+
+;; 20211220T202352+0000
+(define-public lazygit
+  (package
+    (name "lazygit")
+    (version "0.31.4")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/jesseduffield/lazygit")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "198lpyc1qram3nvq79zy806dlb5q7dix58rc3pq22vw4li8vhdyb"))))
+    (build-system go-build-system)
+    (arguments '(#:import-path "github.com/jesseduffield/lazygit"))
+    (propagated-inputs
+     (list go-github-com-atotto-clipboard
+           go-github-com-aybabtme-humanlog
+           go-github-com-cli-safeexec
+           go-github-com-cloudfoundry-jibber-jabber
+           go-github-com-creack-pty
+           go-github-com-fatih-color
+           go-github-com-fsnotify-fsnotify
+           go-github-com-go-errors-errors
+           go-github-com-go-logfmt-logfmt
+           go-github-com-golang-collections-collections
+           go-github-com-golang-protobuf
+           go-github-com-google-go-cmp
+           go-github-com-gookit-color
+           go-github-com-imdario-mergo
+           go-github-com-integrii-flaggy
+           go-github-com-jesseduffield-go-git-v5
+           go-github-com-jesseduffield-gocui
+           go-github-com-jesseduffield-minimal-gitignore
+           go-github-com-jesseduffield-yaml
+           go-github-com-kardianos-osext
+           go-github-com-konsorten-go-windows-terminal-sequences
+           go-github-com-kylelemons-godebug
+           go-github-com-kyokomi-emoji-v2
+           go-github-com-lucasb-eyer-go-colorful
+           go-github-com-mattn-go-colorable
+           go-github-com-mattn-go-runewidth
+           go-github-com-mgutz-str
+           go-github-com-onsi-ginkgo
+           go-github-com-onsi-gomega
+           go-github-com-sahilm-fuzzy
+           go-github-com-sirupsen-logrus
+           go-github-com-spkg-bom
+           go-github-com-stretchr-testify
+           go-github-com-xo-terminfo
+           go-golang-org-x-crypto
+           go-golang-org-x-net
+           go-golang-org-x-sys
+           go-golang-org-x-term
+           go-golang-org-x-text
+           go-gopkg-in-ozeidan-fuzzy-patricia-v3
+           go-github-com-openpeedeep-xdg))
+    (home-page "https://github.com/jesseduffield/lazygit")
+    (synopsis "Table of contents")
+    (description
+     "This package provides a simple terminal UI for git commands, written in Go with
+the @url{https://github.com/jroimartin/gocui,gocui} library.")
+    (license license:expat)))
