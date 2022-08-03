82988cf1692f29c455a9a92658d198fba6f004ca
Sharlatan Hellseher <sharlatanus@gmail.com>
2022-08-03 22:52:34 +0100

    WIP VCS related packages

--- ffab/packages/version-control.scm
+++ ffab/packages/version-control.scm
@@ -18,6 +18,7 @@
 
 (define-module (ffab packages version-control)
   #:use-module (ffab packages golang)
+  #:use-module (ffab packages protobuf)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (gnu packages base)
   #:use-module (gnu packages golang)
@@ -61,7 +62,7 @@
            go-github-com-cpuguy83-go-md2man-v2
            go-github-com-creack-pty
            go-github-com-gabriel-vasile-mimetype
-           go-github-com-google-go-cmp
+           go-github-com-google-go-cmp-cmp
            go-github-com-google-shlex
            go-github-com-hashicorp-go-version
            go-github-com-henvic-httpretty
@@ -102,7 +103,7 @@ with @code{git} and your code.")
 (define-public lazygit
   (package
     (name "lazygit")
-    (version "0.31.4")
+    (version "0.34.0") ; 0.35+ require Golang 1.18+
     (source
      (origin
        (method git-fetch)
@@ -120,13 +121,8 @@ with @code{git} and your code.")
            go-github-com-cli-safeexec
            go-github-com-cloudfoundry-jibber-jabber
            go-github-com-creack-pty
-           go-github-com-fatih-color
            go-github-com-fsnotify-fsnotify
            go-github-com-go-errors-errors
-           go-github-com-go-logfmt-logfmt
-           go-github-com-golang-collections-collections
-           go-github-com-golang-protobuf
-           go-github-com-google-go-cmp
            go-github-com-gookit-color
            go-github-com-imdario-mergo
            go-github-com-integrii-flaggy
@@ -135,30 +131,53 @@ with @code{git} and your code.")
            go-github-com-jesseduffield-minimal-gitignore
            go-github-com-jesseduffield-yaml
            go-github-com-kardianos-osext
-           go-github-com-konsorten-go-windows-terminal-sequences
-           go-github-com-kylelemons-godebug
            go-github-com-kyokomi-emoji-v2
            go-github-com-lucasb-eyer-go-colorful
-           go-github-com-mattn-go-colorable
            go-github-com-mattn-go-runewidth
            go-github-com-mgutz-str
-           go-github-com-onsi-ginkgo
-           go-github-com-onsi-gomega
+           go-github-com-openpeedeep-xdg
+           go-github-com-pmezard-go-difflib
            go-github-com-sahilm-fuzzy
+           go-github-com-sanity-io-litter
            go-github-com-sirupsen-logrus
            go-github-com-spkg-bom
            go-github-com-stretchr-testify
            go-github-com-xo-terminfo
-           go-golang-org-x-crypto
-           go-golang-org-x-net
-           go-golang-org-x-sys
-           go-golang-org-x-term
-           go-golang-org-x-text
-           go-gopkg-in-ozeidan-fuzzy-patricia-v3
-           go-github-com-openpeedeep-xdg))
+           go-gopkg-in-ozeidan-fuzzy-patricia-v3))
     (home-page "https://github.com/jesseduffield/lazygit")
-    (synopsis "Table of contents")
+    (synopsis "Simple terminal UI for git commands")
     (description
      "This package provides a simple terminal UI for git commands, written in Go with
 the @url{https://github.com/jroimartin/gocui,gocui} library.")
     (license license:expat)))
+
+;; 20220803T200717+0100
+(define-public hut
+  (package
+   (name "hut")
+   (version "0.2.0")
+   (source
+    (origin
+     (method git-fetch)
+     (uri (git-reference
+           (url "https://git.sr.ht/~emersion/hut")
+           (commit (string-append "v" version))))
+     (file-name (git-file-name name version))
+     (sha256
+      (base32 "0ybngrwwmkm00dlkdhvkfcvcjhp5xzs8fh90zqr0h12ssqx9pll3"))))
+   (build-system go-build-system)
+   (arguments
+    '(#:import-path "git.sr.ht/~emersion/hut"))
+   (propagated-inputs
+    (list go-git-sr-ht-emersion-go-scfg
+          go-git-sr-ht-emersion-gqlclient
+          go-github-com-google-shlex
+          go-github-com-juju-ansiterm
+          go-github-com-spf13-cobra-1.5
+          go-golang-org-x-oauth2
+          go-golang-org-x-term))
+   (home-page "https://git.sr.ht/~emersion/hut")
+   (synopsis #f)
+   (description "This package provides a CLI tool for
+@url{https://sr.ht/~sircmpwn/sourcehut/,sr.ht}.")
+    (license license:agpl3)))
