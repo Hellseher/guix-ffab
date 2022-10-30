aabd599ecb537df972c821056e47790cf362ba8d
Sharlatan Hellseher <sharlatanus@gmail.com>
2022-10-30 20:37:21 +0000

    ffab: Add easyexif
    * ffab/packages/photo.scm (easyexif): New variable.

--- ffab/packages/photo.scm
+++ ffab/packages/photo.scm
@@ -19,6 +19,8 @@
 (define-module (ffab packages photo)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix build-system cmake)
+  #:use-module (guix build-system gnu)
+  #:use-module (guix gexp)
   #:use-module (guix git-download)
   #:use-module (guix packages))
 
@@ -45,3 +47,47 @@
     (description
      "This package provides RawTherapee's highly optimized RAW processing routines.")
     (license license:gpl3)))
+
+;; 20221029T233737+0100
+(define-public easyexif
+  (package
+    (name "easyexif")
+    (version "1.0")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/mayanklahiri/easyexif")
+                    (commit (string-append "v" version))))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "0l3zr2gzjw25k7gw8z7cpz4prqzfrrpqdqd8gqw2py8pbsxkx8ap"))))
+    (build-system gnu-build-system)
+    (arguments
+     (list #:phases #~(modify-phases %standard-phases
+                        (delete 'configure)
+                        (replace 'check
+                          (lambda _
+                            (invoke "./test.sh")))
+                        (replace 'install
+                          (lambda* (#:key outputs #:allow-other-keys)
+                            (let* ((out (assoc-ref outputs "out"))
+                                   (bin (string-append out "/bin"))
+                                   (share (string-append out "/share"))
+                                   (include (string-append out
+                                             "/include/easyexif")))
+                              (for-each (lambda (dir)
+                                          (mkdir-p dir))
+                                        (list bin include share))
+                              (install-file "demo" bin)
+                              (install-file "exif.cpp" include)
+                              (install-file "exif.h" include)
+                              (install-file "LICENSE" share)))))))
+    (synopsis "ISO-compliant C++ EXIF parsing library")
+    (home-page "https://github.com/mayanklahiri/easyexif")
+    (description
+     "EasyEXIF is a tiny, lightweight C++ library that parses basic information
+out of JPEG files. It uses only the std::string library and is otherwise pure
+C++. You pass it the binary contents of a JPEG file, and it parses several of
+the most important EXIF fields for you.")
+    (license license:bsd-0)))
