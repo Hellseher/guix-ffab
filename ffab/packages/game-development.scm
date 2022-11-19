12728a6fe6de2aae740aae29c4ec7d5d3396c287
Sharlatan Hellseher <sharlatanus@gmail.com>
2022-11-19 20:50:54 +0000

    ffab: cl-virality: Fix build, update to v0.3.0
    * ffab/packages/game-development.scm: (sbcl-virality): Update to v0.3.0
      [arguments](asd-systems): Add virality, vorigin, vorigin.test,
      vshadow, vumbra, vutils.
      [inputs]: Remove sbcl-golden-utils, sbcl-origin, sbcl-shadow,
      sbcl-umbra. Add sbcl-glsl-packing, sbcl-serapeum, sbcl-varjo.
      [description]: Add full annotation from GitHub README.

--- ffab/packages/game-development.scm
+++ ffab/packages/game-development.scm
@@ -18,55 +18,81 @@
 
 (define-module (ffab packages game-development)
   #:use-module (ffab packages lisp-xyz)
+  #:use-module (ffab packages lisp-check)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (gnu packages lisp)
   #:use-module (gnu packages lisp-xyz)
+  #:use-module (gnu packages lisp-check)
   #:use-module (guix build-system asdf)
   #:use-module (guix download)
   #:use-module (guix git-download)
   #:use-module (guix packages))
 
+;; 20221119T195802+0000
 (define-public sbcl-virality
-  (let ((commit "96ea9dd5448bd26388bbaeee115e12a2678005e5")
-        (revision "2"))
-    (package
-      (name "sbcl-virality")
-      (version (git-version "1.0.0" revision commit))
-      (source
-       (origin
-         (method git-fetch)
-         (uri (git-reference
-               (url "https://github.com/bufferswap/ViralityEngine")
-               (commit commit)))
-         (file-name (git-file-name "virality" version))
-         (sha256
-          (base32 "1qh3s8p4hnzbf5a6b9syp2hjd3nz083qgwpjmkddb8dl6yvvy1rj"))))
-      (build-system asdf-build-system/sbcl)
-      (inputs
-       (list sbcl-3b-bmfont
-             sbcl-babel
-             sbcl-cl-cpus
-             sbcl-cl-graph ;; not packed
-             sbcl-cl-opengl
-             sbcl-cl-ppcre
-             sbcl-cl-slug
-             sbcl-closer-mop
-             sbcl-fast-io
-             sbcl-global-vars
-             sbcl-golden-utils
-             sbcl-jsown
-             sbcl-lparallel
-             sbcl-origin
-             sbcl-pngload
-             sbcl-printv
-             sbcl-queues
-             sbcl-sdl2
-             sbcl-shadow
-             sbcl-split-sequence
-             sbcl-static-vectors
-             sbcl-umbra
-             sbcl-trivial-features))
-      (home-page "https://github.com/bufferswap/ViralityEngine")
-      (synopsis "Component-based game engine written in Common Lisp")
-      (description "")
-      (license license:expat))))
+  (package
+    (name "sbcl-virality")
+    (version "0.3.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/bufferswap/ViralityEngine")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name "virality" version))
+       (sha256
+        (base32 "0hvjcvyd628jh4if6swk1wrfb9qdlnpk9ax1y3jarr8ms7ghfcdb"))))
+    (build-system asdf-build-system/sbcl)
+    (arguments
+     `(#:asd-systems '("virality"
+                       "vorigin"
+                       "vorigin.test"
+                       "vshadow"
+                       "vumbra"
+                       "vutils")))
+    (inputs
+     (list sbcl-3b-bmfont
+           sbcl-babel
+           sbcl-cl-cpus
+           sbcl-cl-graph
+           sbcl-cl-opengl
+           sbcl-cl-ppcre
+           sbcl-cl-slug
+           sbcl-closer-mop
+           sbcl-fast-io
+           sbcl-global-vars
+           sbcl-glsl-packing
+           sbcl-jsown
+           sbcl-lparallel
+           sbcl-pngload
+           sbcl-printv
+           sbcl-queues
+           sbcl-sdl2
+           sbcl-serapeum
+           sbcl-split-sequence
+           sbcl-static-vectors
+           sbcl-trivial-features
+           sbcl-varjo))
+    (native-inputs
+     (list sbcl-alexandria sbcl-confidence))
+    (home-page "https://github.com/bufferswap/ViralityEngine")
+    (synopsis "Component-based game engine written in Common Lisp")
+    (description
+     "Virality Engine provides a system and workflow that helps describe the
+elements needed to write 2D or 3D games.  It was designed with several domain
+specific languages that make it easier to describe, manipulate, and use assets
+commonly found in game making.  Such assets include (but are not limited to)
+textures, materials, shader programs, and scene trees of actors that are
+available for instantiation.  Virality Engine also knows how to accept input
+from keyboards and most joysticks and gamepads.
+
+The component system is a hybrid model between an ECS and an object model.  The
+components are defined similar to CLOS defclass, and regular generic methods
+can be used with them.  Components are added to Actors which represent game
+concepts like players, scenery, effects, etc.  We define a component protocol
+invoked by Virality Engine to move your components to the next state and
+render them each frame.")
+    (license license:expat)))
+
+(define-public cl-virality
+  (sbcl-package->cl-source-package sbcl-virality))
