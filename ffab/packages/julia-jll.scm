c8636b13d016f1e958bfb6205c7f3c10a97a318f
Sharlatan Hellseher <sharlatanus@gmail.com>
2023-02-11 21:08:03 +0000

    ffab: [guix:60793]: julia-cfitsio-jll: Mark as accepted.

--- ffab/packages/julia-jll.scm
+++ ffab/packages/julia-jll.scm
@@ -25,48 +25,10 @@
   #:use-module (guix git-download)
   #:use-module (guix packages))
 
-(define-public julia-cfitsio-jll
-  (package
-    (name "julia-cfitsio-jll")
-    ;; NOTE: (Sharlatan-20221204T093314+0000): Update the version after updating
-    ;; main astronomy::cfitsio package.
-    (version "4.0.0+0")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://github.com/JuliaBinaryWrappers/CFITSIO_jll.jl")
-             (commit (string-append "CFITSIO-v" version))))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32 "1k0mqmpyfjr3ibcmda08llw8m166zw0n3lh5y5aj81q37lrxw990"))))
-    (build-system julia-build-system)
-    (arguments
-     '(#:tests? #f ; no runtests
-      #:phases
-      (modify-phases %standard-phases
-          (add-after 'link-depot 'override-binary-path
-            (lambda* (#:key inputs #:allow-other-keys)
-              (map
-               (lambda (wrapper)
-                 (substitute* wrapper
-                   (("using LibCURL_jll") "") ; We use Guix's cfitsio
-                   (("using Zlib_jll") "") ; Not require to upnpack not donwloaded archvie.
-                   (("generate_init_header.*") "generate_init_header()\n" )
-                   (("generate_wrapper_header.*")
-                    (string-append
-                     "generate_wrapper_header(\"CFITSIO\", \""
-                     (assoc-ref inputs "cfitsio") "\")\n"))))
-               ;; There's a Julia file for each platform, override them all
-               (find-files "src/wrappers/" "\\.jl$")))))))
-    (inputs
-     (list cfitsio))
-    (propagated-inputs
-     (list julia-jllwrappers))
-    (home-page "https://github.com/JuliaBinaryWrappers/CFITSIO_jll.jl")
-    (synopsis "cfitsio library wrappers")
-    (description "This package provides a wrapper for the cfitsio library.")
-    (license license:expat)))
+;; 20221204T093314+0000
+;; (define-public julia-cfitsio-jll
+;; added-to-upstream 1973ddbb6c6255c67e256efbaeaccd1dc4587102
+;; CommitDate: Wed Feb 8 09:26:21 2023 +0200
 
 (define-public julia-erfa-jll
   (package
