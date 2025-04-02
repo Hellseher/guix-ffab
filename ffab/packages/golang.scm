05c49b58d60eb76792214dc4724ddf211927bdcc
Sharlatan Hellseher <sharlatanus@gmail.com>
2025-04-02 20:58:48 +0100

    ffab: go-github-com-kyokomi-emoji-v2: Update to 2.2.13.
    * ffab/packages/golang.scm (go-github-com-kyokomi-emoji-v2): Update to
    2.2.13. [native-inputs]: Add go-github-com-puerkitobio-goquery.

--- ffab/packages/golang.scm
+++ ffab/packages/golang.scm
@@ -1714,7 +1714,7 @@ functionality found in `strings` or  `strconv`.")
 (define-public go-github-com-kyokomi-emoji-v2
   (package
     (name "go-github-com-kyokomi-emoji-v2")
-    (version "2.2.10")
+    (version "2.2.13")
     (source (origin
               (method git-fetch)
               (uri (git-reference
@@ -1723,10 +1723,13 @@ functionality found in `strings` or  `strconv`.")
               (file-name (git-file-name name version))
               (sha256
                (base32
-                "1wd9nv1g5cn32sm03lnqvcg4mklb9rp5iwbk0ksabqvzpjwy5mp4"))))
+                "1drnk289m4b24h06yxn860yj81pp2aky218myi71q92inf2sdjgg"))))
     (build-system go-build-system)
     (arguments
      '(#:import-path "github.com/kyokomi/emoji/v2"))
+    (native-inputs
+     ;; For CLI
+     (list go-github-com-puerkitobio-goquery))
     (home-page "https://github.com/kyokomi/emoji")
     (synopsis "Emoji terminal output for Golang")
     (description "This package provides emoji support for termnial output.")
